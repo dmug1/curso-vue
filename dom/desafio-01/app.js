a30011cfe61cbf7b88aec6312980beb3bf9b2ba4
Diego <diegomugnae@gmail.com>
2020-03-17 21:44:12 +1000

    First challenge

---
+++ dom/desafio-01/app.js
@@ -0,0 +1,20 @@
+new Vue ({
+    el: '#desafio',
+    data: {
+        nome:  'Diego',
+        idade: 30,
+        imgGato: src="_MG_9330.jpg"
+    },
+    methods: {
+        multiplicar: function(){
+            idadeMult = this.idade * 3;
+            return idadeMult;
+        },
+        numeroRandom: function () {
+            return Math.floor(Math.random() * Math.floor(2));
+        },   
+        alterarTitulo(){
+           event.target.value =  this.nome
+        }
+    }
+})
