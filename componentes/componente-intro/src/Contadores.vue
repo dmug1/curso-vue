fc01ce0317fedb26121c13ff6e87c2b6f0e1a18f
Diego <diegomugnae@gmail.com>
2020-04-04 13:02:22 +1000

    Use compnents

---
+++ componentes/componente-intro/src/Contadores.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="contadores">
+        <app-contador v-for="c in 2" :key='c'>   </app-contador>
+    </div>
+</template>
+
+<script>
+import Contador from './contador.vue'
+
+export default {
+  components: {
+      'app-contador': Contador
+  }
+}
+
+</script>
+
+<style>
+
+</style>
